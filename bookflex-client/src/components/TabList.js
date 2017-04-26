15d1ed67827305fb7a352388abf96d3a6890b118
joy <unini056378@gmail.com>
2017-04-26 23:55:21 +0900

    feat: connect interpark open api and render bookList and book component

--- bookflex-client/src/components/TabList.js
+++ bookflex-client/src/components/TabList.js
@@ -4,12 +4,14 @@
 import React, { Component } from 'react';
 
 import Tab from './Tab';
+import SearchBar from './SearchBar';
 
 export default class TabList extends Component {
   render() {
     return (
       <div className="tabList">
         <Tab link="/" name="Main" />
+        <SearchBar />
       </div>
     );
   }
--- bookflex-client/src/reducers/index.js
+++ bookflex-client/src/reducers/index.js
@@ -2,8 +2,10 @@
  * Created by Joy on 2017. 4. 24..
  */
 import { combineReducers } from 'redux';
+import BookReducer from './books.reducer';
 
 const rootReducer = combineReducers({
+  books: BookReducer
 });
 
 export default rootReducer;
