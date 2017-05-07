26c56f4e9c532db493c9e757edc80137f106059f
wakoong <woosik.koong@gmail.com>
2017-05-07 21:27:22 +0900

    feature: push bookinfo into the mysql database

--- bookflex-crawling/model/index.js
+++ bookflex-crawling/model/index.js
@@ -10,22 +10,55 @@ const connection = mysql.createConnection({
   user: env.mysql.user,
   password: env.mysql.password,
   database: env.mysql.database
+});
+
+connection.connect(function(error) {
+    if (error) {
+        console.log("Error");
+    } else {
+        console.log("Connected");
+    }
+});
+
+
+
+// for (let i = 0; i < bookInfo.length; i++) {
+//let blogInfo = `INSERT INTO Posts VALUES (default, ${bookInfo[i].title}, ${bookInfo[i].author}, ${bookInfo[i].content}, ${bookInfo[i].imgurl}, ${bookInfo[i].releaseDate})`;
+
+// CREATE TABLE Posts (
+//     postId BIGINT(30) NOT NULL AUTO_INCREMENT,
+//     title VARCHAR(2000) NOT NULL,
+//     author VARCHAR(2000) NOT NULL,
+//     content VARCHAR(2000) NOT NULL,
+//     postImg VARCHAR(2000),
+//     releasedDate VARCHAR(100),
+//  	PRIMARY KEY(postId)
+// );
+
+
+let blogQuery = "INSERT INTO Posts (title, author, content, postImg, releasedDate) VALUES ?";
+let data = bookInfo.map((val)=>{
+    let arr = [];
+    arr.push(val.title);
+    arr.push(val.author);
+    arr.push(val.content);
+    arr.push(val.imgurl);
+    arr.push(val.releaseDate);
+    
+   
+    return arr;
+    
 })
+//console.log(data);
 
-connection.connect();
-
-var blogInfo = `INSERT INTO Posts VALUES (DEFAULT, bookInfo[0].title, bookInfo[0].author, bookInfo[0].content, bookInfo[0].imgurl, bookInfo[0].releaseDate)`;
-
-//for (let i = 0; i < bookInfo.length; i++) {
-connection.query(blogInfo, function(err, rows) {
+connection.query(blogQuery, [data], function(err, rows) {
     if (err) {
             console.log("error")
             throw err;
     } 
     console.log("Book title");
     })
-//}
+// }
 console.log("hello")
 console.log(bookInfo[0].releaseDate);
 
-app.listen('3301');
