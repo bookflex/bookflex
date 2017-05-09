943c409e5382d1516c2acfe376ca5222b061bc26
wakoong <woosik.koong@gmail.com>
2017-05-09 16:48:26 +0900

    chore: resolve conflict

--- bookflex-server/model/index.js
+++ bookflex-server/model/index.js
@@ -1,15 +1,10 @@
 /**
  * Created by Joy on 2017. 5. 1..
  */
-<<<<<<< HEAD
-const mysql = require('mysql');
-const env = require('../config/index');
 
-=======
 import mysql from 'mysql';
-
 import env from '../config/index';
->>>>>>> develop
+
 import MysqlWrapper from './MysqlWrapper';
 
 const connection = mysql.createConnection({
@@ -26,14 +21,6 @@ const getBooksByUserId = (userId) => {
   return mysqlWrapper.execute(`SELECT b.* FROM Books b INNER JOIN BookUserMap bu ON b.bookId = bu.bookId WHERE bu.userId = ${userId}`);
 };
 
-<<<<<<< HEAD
-const getPostsByPostId = () => {
-  return mysqlWrapper.execute(`SELECT * FROM Posts WHERE PostId < 11`);
-}
-
-export default {
-  connection, getBooksByUserId, getPostsByPostId
-=======
 const insertEachBookInfo = (book) => {
   return mysqlWrapper.execute(`SELECT isbn FROM Books WHERE Books.isbn = '${book.isbn}'`).then(result => {
     if(result.length === 0) {
@@ -46,10 +33,12 @@ const insertEachBookInfo = (book) => {
   });
 }
 
-export default {
-  connection, getBooksByUserId, insertEachBookInfo
->>>>>>> develop
+const getPostsByPostId = () => {
+  return mysqlWrapper.execute(`SELECT * FROM Posts WHERE PostId < 11`);
 }
 
+export default {
+  connection, getBooksByUserId, getPostsByPostId
+}
 
 
