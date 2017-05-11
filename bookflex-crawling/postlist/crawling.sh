c8dd2034e58cd40916de81453033894baf11c080
wakoong <woosik.koong@gmail.com>
2017-05-11 23:39:20 +0900

    chore: fix scheduling codes for web scrape

--- bookflex-crawling/postlist/crawling.sh
+++ bookflex-crawling/postlist/crawling.sh
@@ -1,7 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 node ./bookurl.js >> ./crawlingLog
 node ./bookinfo.js >> ./crawlingLog
 node ./bookblog.js >> ./crawlingLog
-# node ./model/index.js >> ./crawlingLog
+node ../model/index.js >> ./crawlingLog
 
+#* * * * 1 /Users/woosikoosi/Desktop/Programming/Codesquad_Blue/bookflex/bookflex-crawling/postlist/crawling.sh
+#crontab-e
