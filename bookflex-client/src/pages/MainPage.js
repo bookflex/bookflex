96683126902b17a5bdd37cc24f1f7184f328f197
joy <unini056378@gmail.com>
2017-05-12 12:15:31 +0900

    chore: resolve conflict

--- bookflex-client/src/pages/MainPage.js
+++ bookflex-client/src/pages/MainPage.js
@@ -3,7 +3,6 @@
  */
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-//import { bindActionCreator } from 'react-redux';
 import { fetchMainPage } from '../actions'
 import BestsellerList from '../components/BestsellerList';
 import PostList from '../components/PostList';
